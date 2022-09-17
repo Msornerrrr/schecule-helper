6f852ff5bd514d763a4bbffccb5b67b3b7f97e66
Richard Jiang <hjiang86@usc.edu>
2022-09-17 03:03:41 -0700

    organize codes

---
+++ db/target.js
@@ -0,0 +1,15 @@
+const courseList = {
+    "math-425a": {
+        "39645": 0
+    },
+    "math-407": {
+        "39625": 0,
+        "39629": 1
+    },
+    "math-430": {
+        "39650": 0,
+        "39651": 1
+    }
+}
+
+module.exports = courseList; 
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const sendMessage = require('./send');
-const scrapeCourse = require('./scrape');
+const sendMessage = require('./util/send');
+const scrapeCourse = require('./util/scrape');
+const courseList = require('./db/target');
 
 require('dotenv').config();
 
@@ -7,46 +8,69 @@ require('dotenv').config();
 const main = async () => {
     // some global setting
     const term = "20223";
+    const baseUrl = `https://classes.usc.edu/term-${term}/course/`;
 
-    // grab course data from user
+    // user expect spots -> 0
+    // user expect pros -> 1
 
-    const course = "math-425a";
-    const section = "39645";
+    // for each course
+    for(course in courseList){
+        // fetch the course data from usc website
+        const sectionList = await scrapeCourse(baseUrl + course);
 
-    const url = `https://classes.usc.edu/term-${term}/course/${course}/`;
-    const sectionList = await scrapeCourse(url);
-    const { professor, type, current, all, available } = sectionList[section];
+        // for each section of a course
+        for(section in courseList[course]){
+            // whether if spots or if professor available
+            const triggerType = courseList[course][section];
+            const { professor, type, current, all, available } = sectionList[section];
 
-    /* check if profesor available
-    if(professor){
-        const title = `Professor ${professor} for ${course.toUpperCase()}`;
-        const message = `
-            Course Info: ${course.toUpperCase()}\n
-            Course Section: ${section}\n
-            Course Instructor: ${professor}\n
-            Course Type: ${type}\n
-            Professor ${professor} is gonna teach ${course.toUpperCase()} this semester for section ${section}\n
-            Click the link to ratemyprofessors to see his/her/they ratings:\n
-            ---> https://www.ratemyprofessors.com/ <---
-        `;
-        sendMessage(title, message);
+            // when we check if there's available spot
+            if(triggerType == 0){
+                if(available){
+                    // prepare and send messages
+                    const title = `${all - current} spots open for ${course.toUpperCase()}`;
+                    const message = `
+                        Course Info: ${course.toUpperCase()}\n
+                        Course Section: ${section}\n
+                        Course Instructor: ${professor}\n
+                        Course Type: ${type}\n
+                        Click the link to quickly nevigate and register for your course:\n
+                        ---> https://my.usc.edu/ <---
+                        \n
+                        You are automatically unsubscribed from this course, if you wanna re-subscribe, click below:\n
+                        ---> some link <---
+                    `;
+                    sendMessage(title, message);
+
+                    // unsubscribe that course
+                    delete triggerType;
+                }
+            }
+            
+            // when we check if professor is available
+            else if(triggerType == 1){
+                if(professor){
+                    // prepare and send messages
+                    const title = `${professor} for ${course.toUpperCase()}`;
+                    const message = `
+                        Course Info: ${course.toUpperCase()}\n
+                        Course Section: ${section}\n
+                        Course Instructor: ${professor}\n
+                        Course Type: ${type}\n
+                        Professor ${professor} is gonna teach ${course.toUpperCase()} this semester for section ${section}\n
+                        Click the link to ratemyprofessors to see his/her/they ratings:\n
+                        ---> https://www.ratemyprofessors.com/ <---
+                    `;
+                    sendMessage(title, message);
+
+                    // unsubscribe that course
+                    delete triggerType;
+                }
+            }
+        }
     }
-    */
-
-    /* check for available spots
-    if(available){
-        const title = `${all - current} spots open for ${course.toUpperCase()}`;
-        const message = `
-            Course Info: ${course.toUpperCase()}\n
-            Course Section: ${section}\n
-            Course Instructor: ${professor}\n
-            Course Type: ${type}\n
-            Click the link to quickly nevigate and register for your course:\n
-            ---> https://my.usc.edu/ <---
-        `;
-        sendMessage(title, message);
-    }
-    */
+    // update database (targeted courses)
+    console.log(courseList);
 }
 
 main();
